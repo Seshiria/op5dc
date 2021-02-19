61864d232318080a6ce97144a782523582a6fb80
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-02-20 00:41:18 +0800

    尝试规避由于kernel源码引入的问题

--- ci.sh
+++ ci.sh
@@ -66,7 +66,12 @@ make -j$(nproc --all) O=out \
                       SUBARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-android- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH
+                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
+                      CC="aarch64-linux-android-gcc -fno-tree-pre"
+                      #尝试修复以下问题
+                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
+                      #提交把gcc 优化级别设置为-O3，导致CC时报错
+                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
 Releases gcc-debug
 #dc patch
 Patch
@@ -78,5 +83,10 @@ make -j$(nproc --all) O=out \
                       SUBARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-android- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH
+                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
+                     CC="aarch64-linux-android-gcc -fno-tree-pre"
+                      #尝试修复以下问题
+                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
+                      #提交把gcc 优化级别设置为-O3，导致CC时报错
+                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
 Releases "`date +%Y%m%d`gcc-dc"
