e098cb0446eda81ca405084a893a202ee97ce63c
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-02-20 21:14:51 +0800

    切换到llvm工具链

--- ci.sh
+++ ci.sh
@@ -58,35 +58,54 @@ Initsystem
 mkdir releases
 cd ./kernel/
 #gcc build
+#make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
+#                        ARCH=arm64 \
+#                        SUBARCH=arm64
+#make -j$(nproc --all) O=out \
+#                      ARCH=arm64 \
+#                      SUBARCH=arm64 \
+#                      CROSS_COMPILE=aarch64-linux-android- \
+#                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+#                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
+#                      CC="aarch64-linux-android-gcc -fno-tree-pre"
+#                      #尝试修复以下问题
+#                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
+#                      #提交把gcc 优化级别设置为-O3，导致CC时报错
+#                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
+#Releases gcc-debug
+##dc patch
+#Patch
+#make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
+#                        ARCH=arm64 \
+#                        SUBARCH=arm64
+#make -j$(nproc --all) O=out \
+#                      ARCH=arm64 \
+#                      SUBARCH=arm64 \
+#                      CROSS_COMPILE=aarch64-linux-android- \
+#                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+#                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
+#                     CC="aarch64-linux-android-gcc -fno-tree-pre"
+#                      #尝试修复以下问题
+#                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
+#                      #提交把gcc 优化级别设置为-O3，导致CC时报错
+#                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
+#Releases "`date +%Y%m%d`gcc-dc"
+
+#llvm build
 make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
                         ARCH=arm64 \
                         SUBARCH=arm64
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      SUBARCH=arm64 \
-                      CROSS_COMPILE=aarch64-linux-android- \
-                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
-                      CC="aarch64-linux-android-gcc -fno-tree-pre"
-                      #尝试修复以下问题
-                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
-                      #提交把gcc 优化级别设置为-O3，导致CC时报错
-                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
-Releases gcc-debug
-#dc patch
 Patch
-make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
-                        ARCH=arm64 \
-                        SUBARCH=arm64
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       SUBARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-android- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
-                     CC="aarch64-linux-android-gcc -fno-tree-pre"
-                      #尝试修复以下问题
-                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
-                      #提交把gcc 优化级别设置为-O3，导致CC时报错
-                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
-Releases "`date +%Y%m%d`gcc-dc"
+                      PATH=${GITHUB_WORKSPACE}/llvm/bin:$PATH \
+                      CC=clang \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip
+Releases "`date +%Y%m%d`llvm-dc"
