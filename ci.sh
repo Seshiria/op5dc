5ee44a94d18141202d9f3afdac244afd62cc6fe2
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-03-02 16:51:20 +0800

    暂时修复由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入的问题 重新启用gcc编译
    Signed-off-by: Seshiria <57834968+Seshiria@users.noreply.github.com>

--- ci.sh
+++ ci.sh
@@ -73,26 +73,29 @@ cd ./kernel/
 #                      #提交把gcc 优化级别设置为-O3，导致CC时报错
 #                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
 #Releases gcc-debug
+
+
+sed -i "s/^KBUILD_CFLAGS   += -O3/KBUILD_CFLAGS   += -O2/" Makefile
+#尝试修复以下问题
+#由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
+#提交把gcc 优化级别设置为-O3，导致CC时报错
+#../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
+
 ##dc patch
-#Patch
-#make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
-#                        ARCH=arm64 \
-#                        SUBARCH=arm64
-#make -j$(nproc --all) O=out \
-#                      ARCH=arm64 \
-#                      SUBARCH=arm64 \
-#                      CROSS_COMPILE=aarch64-linux-android- \
-#                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-#                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
-#                     CC="aarch64-linux-android-gcc -fno-tree-pre"
-#                      #尝试修复以下问题
-#                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
-#                      #提交把gcc 优化级别设置为-O3，导致CC时报错
-#                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
-#Releases "`date +%Y%m%d`gcc-dc"
+Patch
+make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
+                        ARCH=arm64 \
+                        SUBARCH=arm64
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      SUBARCH=arm64 \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
+Releases "`date +%Y%m%d`gcc-dc-fix"
 
 #llvm build
-Patch
+#Patch
 make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
                         ARCH=arm64 \
                         SUBARCH=arm64
