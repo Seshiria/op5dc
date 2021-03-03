c100344e6e652f8c85b19cd72f9d33cd8ba54572
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-03-03 10:42:54 +0800

    用lineageOS的工具链替换系统源的工具链 + https://github.com/LineageOS/android_prebuilts_build-tools
    Signed-off-by: Seshiria <57834968+Seshiria@users.noreply.github.com>

--- ci.sh
+++ ci.sh
@@ -2,46 +2,13 @@
 #for github actions
 set -eu
 Initsystem(){
-    sudo apt update 
-    sudo apt install -y \
-            gcc \
-            g++ \
-            python \
-            make \
-            texinfo \
-            texlive \
-            bc \
-            bison \
-            build-essential \
-            ccache \
-            curl \
-            flex \
-            g++-multilib \
-            gcc-multilib \
-            git \
-            gnupg \
-            gperf \
-            imagemagick \
-            lib32ncurses5-dev \
-            lib32readline-dev \
-            lib32z1-dev \
-            liblz4-tool \
-            libncurses5-dev \
-            libsdl1.2-dev \
-            libssl-dev \
-            libwxgtk3.0-gtk3-dev \
-            libxml2 \
-            libxml2-utils \
-            lzop \
-            pngcrush \
-            rsync \
-            schedtool \
-            squashfs-tools \
-            xsltproc \
-            zip \
-            zlib1g-dev \
-            unzip \
-            language-pack-zh-hans
+    apt update && \
+    apt install -y \
+        gcc \
+        libssl-dev
+    #ghttps://github.com/LineageOS/android_prebuilts_build-tools
+    export PATH=${GITHUB_WORKSPACE}/android_prebuilts_build-tools/path/linux-x86/:$PATH
+    export PATH=${GITHUB_WORKSPACE}/android_prebuilts_build-tools/linux-x86/bin/:$PATH
 }
 
 Patch(){
@@ -54,26 +21,10 @@ Releases(){
     bash ${GITHUB_WORKSPACE}/zip.sh ${1}
 }
 
+
 Initsystem
 mkdir releases
 cd ./kernel/
-#gcc build
-#make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
-#                        ARCH=arm64 \
-#                        SUBARCH=arm64
-#make -j$(nproc --all) O=out \
-#                      ARCH=arm64 \
-#                      SUBARCH=arm64 \
-#                      CROSS_COMPILE=aarch64-linux-android- \
-#                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-#                      PATH=${GITHUB_WORKSPACE}/aarch64/bin:${GITHUB_WORKSPACE}/arm/bin:$PATH \
-#                      CC="aarch64-linux-android-gcc -fno-tree-pre"
-#                      #尝试修复以下问题
-#                      #由https://github.com/LineageOS/android_kernel_oneplus_msm8998/commit/1dc47f9e7d9de9f28628083ba8b14a0aa8d0c490 引入
-#                      #提交把gcc 优化级别设置为-O3，导致CC时报错
-#                      #../drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c:1211:8: error: 'report' may be used uninitialized in this function [-Werror=maybe-uninitialized]
-#Releases gcc-debug
-
 
 sed -i "s/^KBUILD_CFLAGS   += -O3/KBUILD_CFLAGS   += -O2/" Makefile
 #尝试修复以下问题
@@ -83,6 +34,8 @@ sed -i "s/^KBUILD_CFLAGS   += -O3/KBUILD_CFLAGS   += -O2/" Makefile
 
 ##dc patch
 Patch
+
+#gcc build
 make -j$(nproc --all) O=out lineage_oneplus5_defconfig \
                         ARCH=arm64 \
                         SUBARCH=arm64
