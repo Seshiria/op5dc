000eb944d21c0dedef8556edf1c2af447728b0b9
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-06-14 00:04:53 +0800

    更新AnyKernel3源码和配置文件

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=oneplus5 lineageOS dc kernel
+kernel.string=oneplus5 lineageOS dc kernel by Seshiria @ GitHub
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -17,7 +17,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -33,7 +33,7 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
-## AnyKernel install
+## AnyKernel boot install
 dump_boot;
 
 # begin ramdisk changes
@@ -57,5 +57,21 @@ append_file fstab.tuna "usbdisk" fstab;
 # end ramdisk changes
 
 write_boot;
-## end install
+## end boot install
+
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
 
