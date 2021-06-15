5de233b5c783af9af6d8b20798f63d6948391e40
Seshiria <57834968+Seshiria@users.noreply.github.com>
2021-06-15 17:37:56 +0800

    fix 000eb944 引入的配置文件信息错误

--- anykernel.sh
+++ anykernel.sh
@@ -17,7 +17,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
