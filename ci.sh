e01622fd24b6ea2cda5198799b029046d989c716
Seshiria <57834968+Seshiria@users.noreply.github.com>
2022-06-03 22:25:56 +0800

    [依赖改造]处理变量问题

--- ci.sh
+++ ci.sh
@@ -6,9 +6,10 @@ Initsystem() {
         sudo apt install -y \
             libssl-dev \
             python
-    #ghttps://github.com/LineageOS/android_prebuilts_build-tools
-    export PATH=${GITHUB_WORKSPACE}/android_prebuilts_build-tools/path/linux-x86/:$PATH
-    export PATH=${GITHUB_WORKSPACE}/android_prebuilts_build-tools/linux-x86/bin/:$PATH
+    export PATH="${GITHUB_WORKSPACE}"/android_prebuilts_build-tools-"${PREBUILTS_HASH}"/path/linux-x86/:$PATH
+    export PATH="${GITHUB_WORKSPACE}"/android_prebuilts_build-tools-"${PREBUILTS_HASH}"/linux-x86/bin/:$PATH
+    export PATH="${GITHUB_WORKSPACE}"/$LLVM_TAG/bin:"$PATH"
+
 }
 
 Patch() {
@@ -17,17 +18,17 @@ Patch() {
 }
 Releases() {
     #path to ./kernel/
-    cp -f out/arch/arm64/boot/Image.gz-dtb ../AnyKernel3/Image.gz-dtb
+    cp -f out/arch/arm64/boot/Image.gz-dtb ../AnyKernel3-${ANYKERNEL_HASH}/Image.gz-dtb
     #一天可能提交编译多次
     #用生成的文件的MD5来区分每次生成的文件
-    var=$(md5sum ../AnyKernel3/Image.gz-dtb)
+    var=$(md5sum ../AnyKernel3-${ANYKERNEL_HASH}/Image.gz-dtb)
     md5tab=${var:0:5}
     kernelversion=$(head -n 3 "${GITHUB_WORKSPACE}"/kernel/Makefile | awk '{print $3}' | tr -d '\n')
     buildtime=$(date +%Y%m%d)
     bash "${GITHUB_WORKSPACE}"/zip.sh "${1}"-"${kernelversion}"_testbuild_"${buildtime}"_"${md5tab}"
 }
 #使用指定的anykernel配置文件
-cp "${GITHUB_WORKSPACE}"/anykernel.sh "${GITHUB_WORKSPACE}"/AnyKernel3/anykernel.sh
+cp "${GITHUB_WORKSPACE}"/anykernel.sh "${GITHUB_WORKSPACE}"/AnyKernel3-${ANYKERNEL_HASH}/anykernel.sh
 
 Initsystem
 mkdir releases
@@ -53,7 +54,6 @@ make -j"$(nproc --all)" O=out lineage_oneplus5_defconfig \
     SUBARCH=arm64 \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    PATH="${GITHUB_WORKSPACE}"/llvm/bin:"$PATH" \
     HOSTCC=clang \
     HOSTCXX=clang++ \
     CC=clang \
--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 #处理压缩包内的路径问题
-cd ${GITHUB_WORKSPACE}/AnyKernel3/
+cd ${GITHUB_WORKSPACE}/AnyKernel3-${ANYKERNEL_HASH}/
 zip -r9 ../releases/${1}.zip * -x .git README.md *placeholder
 ls -lh ../releases/
