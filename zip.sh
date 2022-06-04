3b980e7d13a60f2c7b7c6e7f765ae2cb959c4deb
Seshiria <57834968+Seshiria@users.noreply.github.com>
2022-06-04 10:22:52 +0800

    [依赖改造]修复换行符问题 [依赖改造]由外部传递变量

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
-#/bin/bash
+#!/bin/bash
 #处理压缩包内的路径问题
-cd ${GITHUB_WORKSPACE}/AnyKernel3-${ANYKERNEL_HASH}/
-zip -r9 ../releases/${1}.zip * -x .git README.md *placeholder
+cd "${2}" || exit
+zip -r9 ../releases/"${1}".zip * -x .git README.md *placeholder
 ls -lh ../releases/
