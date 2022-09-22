820ebedbe03eb1f7ec179510a5ac0feb48a71583
Seshiria <57834968+Seshiria@users.noreply.github.com>
2022-09-22 22:42:45 +0800

    test 恢复lineageos gcc

--- ci.sh
+++ ci.sh
@@ -55,6 +55,8 @@ make -j"$(nproc --all)" O=out lineage_oneplus5_defconfig \
     ARCH=arm64 \
     SUBARCH=arm64 
 
+export PATH="${GITHUB_WORKSPACE}"/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9-"${AARCH64_GCC_HASH}"/bin:"$PATH"
+export PATH="${GITHUB_WORKSPACE}"/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9-"${ARM_GCC_HASH}"/bin:"$PATH"
 (make -j"$(nproc --all)" O=out \
     ARCH=arm64 \
     SUBARCH=arm64 \
