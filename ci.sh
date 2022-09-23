8a452c1147b672e1bf39c8f31a3f5db0ba754797
Seshiria <57834968+Seshiria@users.noreply.github.com>
2022-09-23 17:52:00 +0800

    传递绝对路径 fix 359ce58125ec2be7a8e3382eadf4506951192f52

--- ci.sh
+++ ci.sh
@@ -32,8 +32,8 @@ Releases() {
     kernelversion=$(head -n 3 "${GITHUB_WORKSPACE}"/android_kernel_oneplus_msm8998-"${KERNEL_HASH}"/Makefile | awk '{print $3}' | tr -d '\n')
     buildtime=$(date +%Y%m%d-%H%M%S)
     bash "${GITHUB_WORKSPACE}"/zip.sh "${1}"-"${kernelversion}"_testbuild_"${buildtime}"_"${md5tab}" "${GITHUB_WORKSPACE}"/AnyKernel3-"${ANYKERNEL_HASH}"
-    touch ../AnyKernel3-${ANYKERNEL_HASH}/buildinfo
-    cat > ../AnyKernel3-${ANYKERNEL_HASH}/buildinfo <<EOF
+    touch "${GITHUB_WORKSPACE}"/AnyKernel3-${ANYKERNEL_HASH}/buildinfo
+    cat > "${GITHUB_WORKSPACE}"/AnyKernel3-${ANYKERNEL_HASH}/buildinfo <<EOF
     buildtime ${buildtime}
     Image.gz-dtb hash ${md5}
 EOF
