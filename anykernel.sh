789325d2dab9e40c8a79fa62d070a7c136616600
Seshiria <57834968+Seshiria@users.noreply.github.com>
2022-12-05 15:23:03 +0800

    恢复boot路径 fix addfa3c7238daed6a34617d4beb5b23bbbbc7e12
    (cherry picked from commit 9f9fd7ea8b33ea00cde624bf294a636382043ec1)

--- anykernel.sh
+++ anykernel.sh
@@ -25,7 +25,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
