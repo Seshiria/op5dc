7ef5e8342e373ada9e1d370c54a956e3c17be858
Seshiria <57834968+Seshiria@users.noreply.github.com>
2023-06-12 22:57:59 +0800

    [kernelsu]应该是转义问题 fix 551a30f1d2970e6b449410d243165a5cef60751c

--- ci.sh
+++ ci.sh
@@ -64,7 +64,7 @@ Patch_ksu() {
         ifndef KSU_GIT_VERSION
         KSU_GIT_VERSION = \$(import_KSU_GIT_VERSION)
         ccflags-y += -DKSU_GIT_VERSION=\$(KSU_GIT_VERSION)
-        $(info "Used KSU_GIT_VERSION imported externally !")
+        \$\(info "Used KSU_GIT_VERSION imported externally !"\)
         endif
 EOF
     fi
