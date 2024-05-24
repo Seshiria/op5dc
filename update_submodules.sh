baec23714675d339e881c1d35dd1a198743a4787
Seshiria <57834968+Seshiria@users.noreply.github.com>
2024-05-25 00:30:00 +0800

    不再获取kernelsu新版本

--- update_submodules.sh
+++ update_submodules.sh
@@ -67,8 +67,6 @@ up_hash=$(get_hash "LineageOS/android_prebuilts_build-tools" "${lineage_branch}.
 compare_hash "PREBUILTS_HASH" "$up_hash"
 up_hash=$(get_hash "LineageOS/android_kernel_oneplus_msm8998" "${lineage_branch}")
 compare_hash "KERNEL_HASH" "$up_hash"
-up_hash=$(get_release_hash "tiann/KernelSU" "main")
-compare_hash "KERNELSU_HASH" "$up_hash"
 up_hash=$(get_hash "osm0sis/AnyKernel3" "master")
 compare_hash "ANYKERNEL_HASH" "$up_hash"
 
